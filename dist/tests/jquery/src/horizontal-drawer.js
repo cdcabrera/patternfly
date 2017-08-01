6fee6641ceb5889d27a7ef68475c53ddf9169dfa
cdcabrera <cdcabrera@redhat.com>
2017-08-01 19:13:32 +0000

    Added files generated by Travis build

---
+++ dist/tests/jquery/src/horizontal-drawer.js
@@ -0,0 +1,34 @@
+/*$(document).ready(function() {
+  // matchHeight the contents of each .card-pf and then the .card-pf itself
+  $(".row-cards-pf > [class*='col'] > .card-pf .card-pf-title").matchHeight();
+  $(".row-cards-pf > [class*='col'] > .card-pf > .card-pf-body").matchHeight();
+  $(".row-cards-pf > [class*='col'] > .card-pf > .card-pf-footer").matchHeight();
+  $(".row-cards-pf > [class*='col'] > .card-pf").matchHeight();
+
+  // Initialize Tooltips
+  $('[data-toggle="tooltip"]').tooltip();
+});*/
+
+
+$(document).ready(function() {
+// matchHeight the contents of each .card-pf and then the .card-pf itself
+  var parentElement = $('#notification');
+
+  var elemOne = parentElement.find(".row-cards-pf > [class*='col'] > .card-pf .card-pf-title"),
+      elemTwo = parentElement.find(".row-cards-pf > [class*='col'] > .card-pf > .card-pf-body"),
+      elemThr = parentElement.find(".row-cards-pf > [class*='col'] > .card-pf > .card-pf-footer"),
+      elemFou = parentElement.find(".row-cards-pf > [class*='col'] > .card-pf");
+
+  eval('console.log(elemOne, elemTwo, elemThr, elemFou)');
+
+  elemOne.matchHeight();
+  elemTwo.matchHeight();
+  elemThr.matchHeight();
+  elemFou.matchHeight();
+
+// initialize tooltips
+  parentElement.find('[data-toggle="tooltip"]').tooltip();
+
+// Initialize the vertical navigation
+  //$().setupVerticalNavigation(true);
+});
